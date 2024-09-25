a33e3f90b8fc31883b4a53743657b77d27849d15
rakthuraga <thuraga@usc.edu>
2024-09-25 10:43:37 -0700

    Leetcode medium

---
+++ longestSubstringWithKMostDistinctCharactersClean.cpp
@@ -0,0 +1,19 @@
+int findLongestSubstring(string s, int k) {
+    unordered_map<char, int> counts;
+    int left = 0, ans = 0;
+    
+    for (int right = 0; right < s.size(); right++) {
+        counts[s[right]]++;
+        while (counts.size() > k) {
+            counts[s[left]]--;
+            if (counts[s[left]] == 0) {
+                counts.erase(s[left]);
+            }
+            left++;
+        }
+        
+        ans = max(ans, right - left + 1);
+    }
+    
+    return ans;
+}
